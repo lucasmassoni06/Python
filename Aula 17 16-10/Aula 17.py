b3f08201554c15b69d2ef866bba289c3579b1ea1
Lucas Massoni <lucas.masson0307@gmail.com>
2025-10-16 09:34:08 -0300

    A

--- Aula 17 16-10/Aula 17.py
+++ Aula 17 16-10/Aula 17.py
@@ -1,3 +1,28 @@
-lista = [3, 1, 5, 6, 2, 4, 7]
+def indice_menor(lista):
+    indice = 0
+    for i in range(len(lista)):
+        if lista[i] < lista[indice]:
+            indice = i
+    return indice
 
-def AchaMenor(lista):
+
+'''def selection_sort(lista):
+    ordenada = []
+    while lista:
+        local_menor = indice_menor(lista)
+        menor = lista.pop(local_menor)
+        ordenada.append(menor)
+    return ordenada
+'''
+def selection_sort(lista):
+    for i in range(len(lista)):
+        local_menor = indice_menor(lista)
+        if lista[i] < lista[local_menor]:
+            aux = lista[i]
+            lista[i] = lista[local_menor]
+            lista[local_menor] = aux
+    return lista
+
+lista = [3, 1, 5, 6, 2, 4, 7]
+lista = selection_sort(lista)
+print(lista)
