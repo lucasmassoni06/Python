3d1aea31fdfac43c039c650d628f2812a19027b3
Lucas Massoni <lucas.masson0307@gmail.com>
2025-10-16 11:35:00 -0300

    a

--- Aula 17 16-10/Aula 17.py
+++ Aula 17 16-10/Aula 17.py
@@ -1,4 +1,6 @@
-def indice_menor(lista):
+#Listar menor para o maior
+
+'''def indice_menor(lista):
     indice = 0
     for i in range(len(lista)):
         if lista[i] < lista[indice]:
@@ -6,23 +8,31 @@ def indice_menor(lista):
     return indice
 
 
-'''def selection_sort(lista):
+def selection_sort(lista):
     ordenada = []
     while lista:
         local_menor = indice_menor(lista)
         menor = lista.pop(local_menor)
         ordenada.append(menor)
     return ordenada
-'''
+
 def selection_sort(lista):
     for i in range(len(lista)):
-        local_menor = indice_menor(lista)
-        if lista[i] < lista[local_menor]:
-            aux = lista[i]
-            lista[i] = lista[local_menor]
-            lista[local_menor] = aux
+        local_menor = indice_menor(lista[i:]) + i
+        aux = lista[i]
+        lista[i] = lista[local_menor]
+        lista[local_menor] = aux
     return lista
 
 lista = [3, 1, 5, 6, 2, 4, 7]
 lista = selection_sort(lista)
 print(lista)
+'''
+
+
+#Busca Binaria
+
+
+def busca_binaria(num):
+    while num !== chute**2:
+        chute = num
